ce7e6693e495a4b858b8901850fe011dabd7d732
Shade <kulakaa073@gmail.com>
2026-05-31 21:55:19 +0300

    feat(sanity): register v2 calculator schemas + top-level sidebar group

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -37,10 +37,22 @@ import {tldrBox} from './blocks/tldrBox'
 import {blogCategoryOption} from './documents/blogCategoryOption'
 import {blogPost} from './documents/blogPost'
 import {budgetBucketOption} from './documents/budgetBucketOption'
+import {calculatorCmsOptions} from './documents/calculatorCmsOptions'
+import {calculatorContentOptions} from './documents/calculatorContentOptions'
+import {calculatorDesignOptions} from './documents/calculatorDesignOptions'
+import {calculatorFeatureOptions} from './documents/calculatorFeatureOptions'
+import {calculatorLanguageOptions} from './documents/calculatorLanguageOptions'
+import {calculatorMaintenanceOptions} from './documents/calculatorMaintenanceOptions'
 import {calculatorOption} from './documents/calculatorOption'
 import {calculatorPreset} from './documents/calculatorPreset'
+import {calculatorPresets} from './documents/calculatorPresets'
+import {calculatorProductComplexityOptions} from './documents/calculatorProductComplexityOptions'
 import {calculatorProjectType} from './documents/calculatorProjectType'
+import {calculatorProjectTypes} from './documents/calculatorProjectTypes'
+import {calculatorSeoGrowthOptions} from './documents/calculatorSeoGrowthOptions'
+import {calculatorSeoOptions} from './documents/calculatorSeoOptions'
 import {calculatorSettings} from './documents/calculatorSettings'
+import {calculatorTimelineOptions} from './documents/calculatorTimelineOptions'
 import {caseStudy} from './documents/caseStudy'
 import {countryOption} from './documents/countryOption'
 import {industryPage} from './documents/industryPage'
@@ -98,4 +110,18 @@ export const schemaTypes = [
   calculatorOption,
   calculatorPreset,
   calculatorSettings,
+
+  // v2 calculator singletons (consolidate v1 docs)
+  calculatorProjectTypes,
+  calculatorPresets,
+  calculatorCmsOptions,
+  calculatorSeoOptions,
+  calculatorFeatureOptions,
+  calculatorLanguageOptions,
+  calculatorDesignOptions,
+  calculatorTimelineOptions,
+  calculatorMaintenanceOptions,
+  calculatorSeoGrowthOptions,
+  calculatorContentOptions,
+  calculatorProductComplexityOptions,
 ]
